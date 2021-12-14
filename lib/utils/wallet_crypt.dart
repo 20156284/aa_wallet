73e73006700d122469e5acad5754e227e1707451
Will <willzh@live.cn>
2021-12-14 15:54:54 +0800

    just update

--- lib/utils/wallet_crypt.dart
+++ lib/utils/wallet_crypt.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:aes_ecb_pkcs5_flutter/aes_ecb_pkcs5_flutter.dart';
 import 'package:convert/convert.dart';
 import 'package:crypto/crypto.dart';
@@ -18,8 +19,12 @@ class WalletCrypt {
     final digest = md5.convert(content);
     final passwordMd5 = hex.encode(digest.bytes);
     try {
+      // final key = Key.fromUtf8(passwordMd5);
+      // final iv = IV.fromLength(16);
+      // final encrypter = Encrypter(AES(key, mode: AESMode.ecb));
       final String res =
           await FlutterAesEcbPkcs5.encryptString(data, passwordMd5);
+
       return res;
     } catch (e) {
       return e.toString();
